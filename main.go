fd1cb07d61588a1e7942beadc7b4f41ee16364f4
Nikkeh <nikkehtine@int.pl>
2023-08-31 15:48:58 +0200

    Add support for arguments

--- main.go
+++ main.go
@@ -6,6 +6,24 @@ import (
 )
 
 func main() {
-	fmt.Println("Hello World")
+	// Check if any arguments were passed
+	if len(os.Args) == 1 {
+		fmt.Println("No arguments passed")
+		os.Exit(1)
+	}
+
+	arguments := os.Args[1:]
+
+	data, err := os.ReadFile(arguments[0])
+	check(err)
+	fmt.Print(string(data))
+
 	os.Exit(0)
+}
+
+// Generalized error handling
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
 }
