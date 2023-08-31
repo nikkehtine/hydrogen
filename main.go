5dc3e52559f77bc34a82e426bca94946f6dfd515
Nikkeh <nikkehtine@int.pl>
2023-08-31 22:32:32 +0200

    Fix lexer logic Add another test file

--- main.go
+++ main.go
@@ -1,16 +1,16 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"os"
+	"unicode"
 )
 
 type TokenType int
 
 const (
-	illegal TokenType = iota
-	eof
-	white
+	eof TokenType = iota
 	returns
 	int_lit
 	semi
@@ -18,14 +18,55 @@ const (
 
 type Token struct {
 	Type  TokenType
-	Value *string
+	Value string
 }
 
-func tokenize(data string) {
+func tokenize(data string) []Token {
+	var tokens []Token
+
+	var strBuf bytes.Buffer
 	for i := 0; i < len(data); i++ {
 		var c rune = rune(data[i])
-		fmt.Printf("%d: %c\n", i, c)
+
+		// fmt.Printf("%d: %c\n", i, c)
+
+		if unicode.IsLetter(c) {
+			strBuf.WriteRune(c)
+			i++
+
+			for unicode.IsLetter(rune(data[i])) {
+				c = rune(data[i])
+				strBuf.WriteRune(c)
+				i++
+			}
+			i--
+
+			if strBuf.String() == "return" {
+				tokens = append(tokens, Token{Type: returns})
+				strBuf.Reset()
+			} else {
+				fmt.Printf("%s: No such keyword\n", strBuf.String())
+				os.Exit(1)
+			}
+		} else if unicode.IsDigit(c) {
+			strBuf.WriteRune(c)
+			i++
+			for unicode.IsDigit(rune(data[i])) {
+				strBuf.WriteRune(rune(data[i]))
+				i++
+			}
+			i--
+			tokens = append(tokens, Token{Type: int_lit, Value: strBuf.String()})
+		} else if unicode.IsSpace(c) {
+			continue
+		} else if c == rune(';') {
+			tokens = append(tokens, Token{Type: semi})
+		} else {
+			fmt.Println("Ya dun goofed")
+			os.Exit(1)
+		}
 	}
+	return tokens
 }
 
 func main() {
