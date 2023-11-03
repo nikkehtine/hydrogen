7cf140cd480f1c200b59a55a2abb56888706f851
nikkehtine <nikkehtine@int.pl>
2023-11-03 22:18:10 +0100

    Improve test script

--- test.sh
+++ test.sh
@@ -2,21 +2,46 @@
 # Basic script to quickly test the program
 # Commands: empty, "compile", "run", "cleanup"
 
+
+# Variables
+OUTDIR=tmp
+FILE=$OUTDIR/test
+
+# Helper function
+checkErr() {
+    if [ $? -ne 0 ]; then
+        echo "!!! script failed during $1"
+        exit 1
+    fi
+}
+
+# Actual steps
+
 compile() {
     go run . test.hy
-    nasm -felf64 out.asm
-    ld -o out out.o
+    checkErr "compilation"
+
+    if [ ! -d $OUTDIR ]; then
+        mkdir $OUTDIR
+    fi
+    mv out.asm $FILE.asm
+
+    nasm -felf64 $FILE.asm -o $FILE.o
+    checkErr "assembling"
+
+    ld -o $FILE $FILE.o
+    checkErr "linking"
 }
 
 run() {
-    ./out
+    ./$FILE
     echo "Program exit code: $?"
 }
 
 cleanup() {
-    rm out.asm
-    rm out.o
-    rm out
+    if [ -d $OUTDIR ]; then
+        rm -r $OUTDIR
+    fi
 }
 
 case $1 in
